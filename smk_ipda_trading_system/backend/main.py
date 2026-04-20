6b2237fabc3011e94e401f39e93030d74ff25c24
J Roberto Jimenez <tijuanapaint@gmail.com>
2026-04-19 21:17:57 -0600

    Update main.py

--- smk_ipda_trading_system/backend/main.py
+++ smk_ipda_trading_system/backend/main.py
@@ -247,4 +247,4 @@ async def stream(ws: WebSocket):
             except asyncio.CancelledError: pass
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="10.0.40.5", port=8000, reload=True)
